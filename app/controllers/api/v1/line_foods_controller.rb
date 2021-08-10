481ac8d74e4bd166bf2c7cf6c1a7673839889cfb
seiya_t <kenose0328@gmail.com>
2021-08-11 00:15:30 +0900

    [add]コメント追加

--- app/controllers/api/v1/line_foods_controller.rb
+++ app/controllers/api/v1/line_foods_controller.rb
@@ -18,6 +18,10 @@ module Api
       end
 
       def create
+        # 仮注文がアクティブな商品の店以外の店舗が存在する場合
+        ## その店舗の名前(existing_restaurant)と
+        ## 選択中の商品の店の名前(new_restaurant)を
+        ## json形式、ステータスは406で返す
         if LineFood.active.other_restaurant(@ordered_food.restaurant.id).exists?
           return render json: {
             existing_restaurant: LineFood.other_restaurant(@ordered_food.restaurant.id).first.restaurant.name,
@@ -25,8 +29,13 @@ module Api
           }, status: :not_acceptable
         end
 
+        # 選択中の商品についてactive状態及び個数を更新する => @line_foodに格納
         set_line_food(@ordered_food)
 
+        # 更新された@line_foodを保存し、
+        # 成功した場合はline_foodを、
+        # 失敗した場合はinternal_server_errorを
+        # json形式で返す
         if @line_food.save
           render json: {
             line_food: @line_food
@@ -36,13 +45,19 @@ module Api
         end
       end
 
+      # 既にある古い仮注文を論理削除する
       def replace
         LineFood.active.other_restaurant(@ordered_food.restaurant.id).each do |line_food|
           line_food.update_attribute(:active, false)
         end
 
+        # 選択中の商品についてactive状態及び個数を更新し直す => @line_foodに格納
         set_line_food(@ordered_food)
 
+        # 更新された@line_foodを保存し、
+        # 成功した場合はline_foodを、
+        # 失敗した場合はinternal_server_errorを
+        # json形式で返す
         if @line_food.save
           render json: {
             line_food: @line_food
@@ -54,11 +69,17 @@ module Api
 
       private
 
+      # 選択中の商品を@ordered_foodとする
       def set_food
         @ordered_food = Food.find(params[:food_id])
       end
 
+
+      # 選択中の商品をactiveとした@line_foodを作成する
       def set_line_food(ordered_food)
+        # 選択中の商品が仮注文されている場合
+          # 商品の個数のとアクティブ状態を更新する
+          # そうでない場合は新規に商品と仮注文を関連付ける
         if ordered_food.line_food.present?
           @line_food = ordered_food.line_food
           @line_food.attributes = {
